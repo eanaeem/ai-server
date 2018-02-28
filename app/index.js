62d710d45bbf61cc9f43b8a5482790338f9a2834
eanaeem <eanaeem@gmail.com>
2018-02-28 18:52:47 +0000

    changes

--- app/index.js
+++ app/index.js
@@ -21,11 +21,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 	// optionsSuccessStatus: 200
 
 // }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use(logger());
 
 
