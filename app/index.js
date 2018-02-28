08c64693b825ad9a2444a235e1222c0b1d2b3ec4
eanaeem <eanaeem@gmail.com>
2018-02-28 18:51:12 +0000

    morgan

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
-// import logger from 'morgan';
+import logger from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-// import morgan from 'morgan';
+import morgan from 'morgan';
 
 import routes from './controller';
 import Config from './config/index';
@@ -13,7 +13,7 @@ import Config from './config/index';
 const app = express();
 // app.use(session({secret:'shah'}));
 // app.use(passport.initialize());
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors({
 // 	origin: "http://localhost:3005",
@@ -21,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 	// optionsSuccessStatus: 200
 
 // }));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-// app.use(logger());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(logger());
 
 
 
