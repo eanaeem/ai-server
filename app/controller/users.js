381fdb1daa435cc511672a46521a8c1963a1084e
eanaeem <eanaeem@gmail.com>
2018-03-01 12:48:26 +0000

    updated promise

--- app/controller/users.js
+++ app/controller/users.js
@@ -3,12 +3,16 @@ import User from '../models/userModel'
 const Users = (req, res) => {
   console.log(req.body);
   const { name, username, password } = req.body;
-  if (!name || !username) res.send('Please send name and username');
+  if (!name || !username) { res.send('Please send name and username'); return; }
   let user = new User({ name, username, password });
-  user.save((err, user) => {
-    if (err) console.log('User not saved successfully!', err);
-    res.send({msg: 'user saved successfully', user});
-  });
+  user.save()
+    .then((user) => {
+      res.send({ msg: 'user saved successfully', user });
+    })
+    .catch(err => {
+      console.log(`error saving user: ${err}`);
+      res.send(err);
+    });
 };
 
 export default Users;
